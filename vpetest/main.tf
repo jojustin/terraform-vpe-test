52aa3333be79816ec03752ab021dcff85bfcd824
Josephine Eskaline Joyce <jojustin@in.ibm.com>
2024-04-09 18:37:31 +0530

    vpe-test

--- vpetest/main.tf
+++ vpetest/main.tf
@@ -44,9 +44,12 @@ resource "ibm_resource_instance" "secrets_manager" {
   plan              = "trial"
   location          = var.region
   resource_group_id = data.ibm_resource_group.resource_group.id
-  service_endpoints = "private-only"
+  service_endpoints = "private"
   timeouts {
     create = "20m" # Extending provisioning time to 20 minutes
+  }
+  parameters = {
+    "allowed_network" = "private-only"
   }
 }
 
