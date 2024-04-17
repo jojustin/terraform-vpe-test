9346f48a8b33aa2ed154d3feea6e652de48d2755
Josephine Eskaline Joyce <jojustin@in.ibm.com>
2024-04-18 00:08:58 +0530

    Update main.tf

--- accesskpvpe/main.tf
+++ accesskpvpe/main.tf
@@ -65,7 +65,7 @@ resource "ibm_is_virtual_endpoint_gateway" "endpoint_gateways" {
   name            = "${local.prefix}-vpe"
   # check if target is a CRN and handle accordingly
   target {
-    crn = ibm_resource_instance.kms.crn
+    crn = "crn:v1:bluemix:public:kms:au-syd:::endpoint:private.au-syd.kms.cloud.ibm.com"
     resource_type = "provider_cloud_service"
   }
   vpc = ibm_is_vpc.vpc1.id
