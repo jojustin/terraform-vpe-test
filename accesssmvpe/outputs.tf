c51c92a73992ffca088afd4e4fe634567676abec
Josephine Eskaline Joyce <jojustin@in.ibm.com>
2024-04-17 21:06:24 +0530

    Update outputs.tf

--- accesssmvpe/outputs.tf
+++ accesssmvpe/outputs.tf
@@ -9,15 +9,6 @@
 #   value   = local.kms_config.crk_id
 # }
 
-output "smguid" {
-    value = ibm_resource_instance.secrets_manager.guid
-}
-
-
-output "before" {
-    value = ibm_sm_arbitrary_secret.sm_arbitrary_secret_before.crn
-}
-
 
 # output "afer" {
 #     value = ibm_sm_arbitrary_secret.sm_arbitrary_secret_after.crn
